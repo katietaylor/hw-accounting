3e1a06ef1da101ade0c31e967af9f73444b1a6e5
Katie <knt.taylor@gmail.com>
2017-02-24 04:51:58 +0000

    broke code into three functions for readibility and reuse

--- accounting.py
+++ accounting.py
@@ -3,37 +3,79 @@ INTERNET_INDEX = 1
 DORKY_LINE_LENGTH = 80
 
 print "*" * DORKY_LINE_LENGTH
-f = open("orders-by-type.txt")
-melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
 
-for l in f:
-    data = l.split("|")
-    melon_type = data[1]
-    melon_count = int(data[2])
-    melon_tallies[melon_type] += melon_count
 
-f.close()
-melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
-total_revenue = 0
-for melon_type in melon_tallies:
-    price = melon_prices[melon_type]
-    revenue = price * melon_tallies[melon_type]
-    total_revenue += revenue
-    # print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    print "We sold {} {} melons at {:.2f} each for a total of {:.2f}".format(melon_tallies[melon_type], melon_type, price, revenue)
-print "******************************************"
-f = open("orders-with-sales.txt")
-sales = [0, 0]
-for line in f:
-    d = line.split("|")
-    if d[1] == "0":
-        sales[0] += float(d[3])
+def order_tallies(order_type_file):
+    """ Open file and tally the orders by type
+    """
+
+    orders = open(order_type_file)
+
+    melon_tallies = {
+        "Musk": 0,
+        "Hybrid": 0,
+        "Watermelon": 0,
+        "Winter": 0
+        }
+
+    for line in orders:
+        data = line.split("|")
+        melon_type = data[1]
+        melon_count = int(data[2])
+        melon_tallies[melon_type] += melon_count
+
+    orders.close()
+
+    return melon_tallies
+
+
+def melon_revenue(melon_tallies):
+    """calculate revenue
+    """
+
+    melon_prices = {
+        "Musk": 1.15,
+        "Hybrid": 1.30,
+        "Watermelon": 1.75,
+        "Winter": 4.00
+        }
+
+    total_revenue = 0
+
+    print "MELON SALES DATA"
+
+    for melon_type in melon_tallies:
+        price = melon_prices[melon_type]
+        revenue = price * melon_tallies[melon_type]
+        total_revenue += revenue
+
+        print "We sold {} {} melons at {:.2f} each for a total of {:.2f}" .format(
+            melon_tallies[melon_type], melon_type, price, revenue)
+
+
+def sales_team_performance(sales_file):
+    """ Calculate and compare sales team to online sales.
+    """
+
+    sales_type = open(sales_file)
+    sales_team = 0
+    online = 0
+
+    for line in sales_type:
+        data = line.split("|")
+        if data[1] == "0":
+            online += float(data[3])
+        else:
+            sales_team += float(data[3])
+    print "SALES TYPE DATA"
+    print "Salespeople generated ${:.2f} in revenue.".format(sales_team)
+    print "Internet sales generated ${:.2f} in revenue.".format(online)
+    if sales_team > online:
+        print "Guess there's some value to those salespeople after all."
     else:
-        sales[1] += float(d[3])
-print "Salespeople generated ${:.2f} in revenue.".format(sales[1])
-print "Internet sales generated ${:.2f} in revenue.".format(sales[0])
-if sales[1] > sales[0]:
-    print "Guess there's some value to those salespeople after all."
-else:
-    print "Time to fire the sales team! Online sales rule all!"
-print "******************************************"
+        print "Time to fire the sales team! Online sales rule all!"
+
+
+melon_tallies = order_tallies("orders-by-type.txt")
+melon_revenue(melon_tallies)
+sales_team_performance("orders-with-sales.txt")
